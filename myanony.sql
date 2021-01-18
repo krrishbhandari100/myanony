3ea397473fcf14df00fe16e1e8f9b739d3882c72
Krrish <krrish@mail2webmaster.com>
2021-01-18 13:19:12 +0530

    Initial commit

--- myanony.sql
+++ myanony.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.6.6deb5ubuntu0.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 03, 2020 at 12:51 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
+-- Host: localhost:3306
+-- Generation Time: Dec 23, 2020 at 05:28 AM
+-- Server version: 5.7.32-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -33,8 +31,17 @@ CREATE TABLE `messages` (
   `roomname` varchar(255) NOT NULL,
   `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `message` text NOT NULL,
-  `ip` varchar(255) NOT NULL
+  `ip` varchar(255) NOT NULL,
+  `join_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`sno`, `roomname`, `time`, `message`, `ip`, `join_name`) VALUES
+(1, 'krrish', '2020-12-23 05:26:57', 'ddd', '49.36.242.150', 'krrish'),
+(2, 'krrish', '2020-12-23 05:28:23', 'sdfdsf', '49.36.242.150', 'krrish');
 
 -- --------------------------------------------------------
 
@@ -49,6 +56,13 @@ CREATE TABLE `rooms` (
   `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `ip_addr` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rooms`
+--
+
+INSERT INTO `rooms` (`sno`, `roomname`, `password`, `time`, `ip_addr`) VALUES
+(1, 'krrish', '0560a8f097e66d67a5026dcf1d54a3c8', '2020-12-23 05:22:32', '49.36.242.150');
 
 --
 -- Indexes for dumped tables
@@ -74,15 +88,12 @@ ALTER TABLE `rooms`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `sno` int(255) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `sno` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `sno` int(255) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+  MODIFY `sno` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
